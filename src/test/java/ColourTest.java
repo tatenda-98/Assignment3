6339be0d8a11dc4130962a0e6eb6747bce499b60
tats98 <60622544+tatenda-98@users.noreply.github.com>
2020-12-15 15:46:47 +0000

    Test to check if the object was instantiated correctly. Failed

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -4,6 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTest {
     @Test
+    public void validObjectInstantiation() {
+        Colour obj1 = new Colour(45, 24, 67);
+        assertTrue((obj1.getR() == obj1.r && obj1.getG() == obj1.g && obj1.getB() == obj1.b), "Object was successfully created");
+    }
+
+    @Test
     public void componentValueCheck() {
         assertThrows(IllegalArgumentException.class, ()-> {
            Colour obj1 = new Colour(-12,46,3);
