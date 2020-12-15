b337ce29ad128ad2d07318ef337768cc877cccac
tats98 <60622544+tatenda-98@users.noreply.github.com>
2020-12-15 15:04:50 +0000

    Test to check for valid component values. Failed

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -3,7 +3,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTest {
-    public static void main(String[] args) {
-        Colour obj1 = new Colour(40, 20, 70);
+    @Test
+    public void componentValueCheck() {
+        assertThrows(IllegalArgumentException.class, ()-> {
+           Colour obj1 = new Colour(-12,46,3);
+        });
     }
 }
